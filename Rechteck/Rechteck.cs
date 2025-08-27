b3d17bde9bcd6dd7d7f83c9ee047eff4deb9a45f
arch-user-france1 <francebaciu@outlook.com>
2025-08-27 13:55:12 +0200

    Add Rechteck.cs

---
+++ Rechteck/Rechteck.cs
@@ -0,0 +1,22 @@
+namespace Rechteck;
+
+public record Rechteck
+{
+    public int Id { get; }
+    public string Name { get; set;  }
+    public double Länge { get; set; }
+    public double Breite { get; set; }
+    public Punkt Position { get; set; }
+
+    public const int Winkelsumme = 360;
+
+    public double Umfang
+    {
+        get => 2 * (Breite + Länge);
+    }
+
+    public record Punkt
+    {
+        
+    }
+}
